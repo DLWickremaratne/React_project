05825074844fc7607019b7988d4e749a03b10490
DLWickremeratne <dhanithlak@gmail.com>
2022-10-20 09:31:29 +0530

    Dependency array

--- react_curd/src/App.js
+++ react_curd/src/App.js
@@ -4,13 +4,19 @@ import { useState , useEffect } from "react";
 
 function App({name}) {
 
-  const [emotion, setEmotion]= useState("I'm happy"); //handle state variable
+  const [emotion, setEmotion]= useState("happy"); //handle state variable
+  const [second, secondEmotion] = useState("Exited");
 
+  //useEffect - manage side effects
 
-  //useEffext - manage side effects
   useEffect(() => {
-    return console.log({emotion});
-  }, [emotion]);
+    console.log(`I'm ${emotion} !`);
+  }, [emotion ]);
+
+  useEffect(() => {
+    console.log(`Now i'm ${second}`)
+  }, [second]);
+
 
   return (
     <div className="App">
@@ -20,8 +26,12 @@ function App({name}) {
         
         <h1>Hi {emotion}</h1>
 
-        <button onClick={() => setEmotion("I'm sad")}>Sad</button>
-        <button onClick={() => setEmotion("I'm Angry")}>Angry</button> 
+        <button onClick={() => setEmotion("sad")}>Sad</button>
+        <button onClick={() => setEmotion("Angry")}>Angry</button> 
+
+        <button onClick={() => secondEmotion("GrateFull")}>GrateFull</button> 
+
+        
 
 
 
